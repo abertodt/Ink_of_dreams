833cea2b89e7df484224cb0b6e9f4f506d955125
albertodt <113683638+abertodt@users.noreply.github.com>
2024-12-04 17:18:13 +0100

    Toon and outline shader, paint mode post process

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -7,11 +7,12 @@ public class CameraFollow : MonoBehaviour
 {
     [Header("CameraSettings")]
     [SerializeField] private float _distanceToPlayer;
+    [SerializeField] private float _height;
     [SerializeField] private float _FOV;
 
     private void Start()
     {
-        transform.position = transform.parent.position + new Vector3(0, transform.position.y, -_distanceToPlayer);
+        transform.localPosition = transform.parent.position + new Vector3(0, _height, -_distanceToPlayer);
         Camera.main.fieldOfView = _FOV;
     }
 
