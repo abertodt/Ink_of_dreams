9168fa1af0ab3f5d5788e121b3543bd024b90b14
albertodt <113683638+abertodt@users.noreply.github.com>
2024-12-16 22:20:20 +0100

    Scene config script fixed for build

--- Assets/Scripts/Utils/ScriptableObjects/ScenesConfiguration/ScenesConfiguration.cs
+++ Assets/Scripts/Utils/ScriptableObjects/ScenesConfiguration/ScenesConfiguration.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SceneConfig", menuName = "ScriptableObjects/SceneConfig", order = 3)]
