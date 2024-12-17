11ce4e1f7c800a9caf66828ccc85302aa5bae5ae
albertodt <113683638+abertodt@users.noreply.github.com>
2024-12-17 09:48:47 +0100

    Splatter effect on object spawn, max objects spawned at a time

--- Assets/Scripts/Player/ObjectSpawner.cs
+++ Assets/Scripts/Player/ObjectSpawner.cs
@@ -1,17 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private GameObject _splatterEffect;
+    [SerializeField] private const int MaxSpawnedObjects = 3;
 
     [Header("Events")]
     [SerializeField] private GameEvent _onObjectSpawn;
+
+    private Queue<GameObject> _spawnedObjects = new Queue<GameObject>();
+    
+
     public void SpawnObject(Component sender, object data)
     {
         if(data is GameObject)
         {
-            Instantiate((GameObject)data, transform.position, transform.rotation);
+            GameObject spawnedObject = Instantiate((GameObject)data, transform.position, transform.rotation);
+
+            if (_splatterEffect != null)
+            {
+                Instantiate(_splatterEffect, transform.position, transform.rotation);
+            }
+
+            _spawnedObjects.Enqueue(spawnedObject);
+
+            if (_spawnedObjects.Count > MaxSpawnedObjects)
+            {
+                GameObject oldestObject = _spawnedObjects.Dequeue();
+                if (oldestObject != null)
+                {
+                    Destroy(oldestObject);
+                }
+            }
+
             //_onObjectSpawn?.Raise(this, data);
             GameManager.Instance.ChangeState(GameState.Gameplay);
         }
